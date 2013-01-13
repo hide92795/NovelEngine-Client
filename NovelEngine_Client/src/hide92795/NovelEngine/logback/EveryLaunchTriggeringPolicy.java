caee6e7e20f8c5c61cfd6b8be7de3dd43749b8f9
hide92795 <hidetugu.0927.zushi@gmail.com>
2013-01-13 16:59:13 +0900

    ログファイルが起動毎にローリングするように

---
+++ NovelEngine_Client/src/hide92795/NovelEngine/logback/EveryLaunchTriggeringPolicy.java
@@ -0,0 +1,28 @@
+package hide92795.novelengine.logback;
+
+import java.io.File;
+
+import ch.qos.logback.core.rolling.TriggeringPolicyBase;
+
+/**
+ * 起動毎にログファイルのローリングを行います。
+ *
+ * @param <E>
+ * @author hide92795
+ */
+public class EveryLaunchTriggeringPolicy<E> extends TriggeringPolicyBase<E> {
+	/**
+	 * ローリングを行ったかどうかを表します。
+	 */
+	private boolean launch = false;
+
+	@Override
+	public final boolean isTriggeringEvent(final File activeFile, final E event) {
+		if (!launch) {
+			launch = true;
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ NovelEngine_Client/src/hide92795/NovelEngine/logback/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <code>Logback</code> についてのクラスを提供します。
+ */
+package hide92795.novelengine.logback;
